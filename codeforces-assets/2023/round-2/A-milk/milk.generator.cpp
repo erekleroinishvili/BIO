e3627f862af55b7c478884f6bb13a5ff43fcd09b
Erekle Roinishvili <erekleroinishvili@gmail.com>
2023-05-30 21:35:17 +0100

    Fix: Milk generator avoids duplicate tubes

--- codeforces-assets/2023/round-2/A-milk/milk.generator.cpp
+++ codeforces-assets/2023/round-2/A-milk/milk.generator.cpp
@@ -13,6 +13,11 @@ using namespace std;
 
 #define H_MAX (1<<12)
 
+pair<int, int> orderedPairOf(int a, int b) {
+    if (a<b) return {a,b};
+    return {b,a};
+}
+
 int main(int argc, char *argv[]) {
     registerGen(argc, argv, 1);
 
@@ -26,23 +31,30 @@ int main(int argc, char *argv[]) {
         ? rnd.next(0, min(1 + 3 * (h-1) / 2, atoi(argv[2]))) // Tube number can be from 0 to its maximum
         : rnd.next(0, 1 + 3 * (h-1) / 2); // Tube number can be from 0 to its maximum
 
+    set<pair<int, int>> tubes; // Existing tubes to make sure tube does not repeat
 
-    // pair<index, resource> Resource is the number of tubes this house can still connect
-    vector<pair<int, int>> node(h); // h houses
-    node[0] = {1, 2}; //  First house can connect two tubes
-    for (int i = 1; i < h; ++i) node[i] = {1 + i, 3}; // All other houses can connect three tubes
+    vector<int> capacity(h); // For each house, how many more tubes it can still connect
+    capacity[0] = 2; // First house can connect two tubes
+    for (int i = 1; i < h; ++i) capacity[i] = 3; // All other houses can connect three tubes
 
     for (int tube = 1; tube <= tubeCount; ++tube) {
-        if ( node.size() < 2 ) break; // At least two nodes must exist.
-        int t1 = rnd.next((int)node.size() - 0); // First candidate chosen freely
-        int t2 = rnd.next((int)node.size() - 1); // Second candidate chosen with a leeway to avoid collision
-        if ( t1 == t2 ) ++t2; // If both candidates are same, change second candidate
-        if ( node[t1].first > node[t2].first ) swap(t1, t2); // Ensure the house numbers are increasing from t1 to t2
-        cout << node[t1].first << ' ' << node[t2].first << endl;
-        if ( --node[t2].second <= 0 ) node.erase(node.begin() + t2);
-        if ( --node[t1].second <= 0 ) node.erase(node.begin() + t1);
+        // Choose house A
+        int t1 = rnd.next(h); // Start by randomly trying a house
+        for (int i = t1, first=1; first || i!=t1 ; t1=(t1+1)%h, first=0) {
+            if ( capacity[t1] ) break;
+        }
+        assert(capacity[t1]);
+        --capacity[t1];
+        // Choose house B
+        int t2 = rnd.next(h); // Second candidate chosen with a leeway to avoid collision
+        for (int i = t2, first=1; first || i!=t2 ; t2=(t2+1)%h, first=0) {
+            if ( capacity[t2] && t1!=t2 && tubes.find(orderedPairOf(t1,t2))==tubes.end() ) break;
+        }
+        assert(capacity[t2] && t1!=t2 && tubes.find(orderedPairOf(t1,t2))==tubes.end());
+        --capacity[t2];
+        tubes.insert(orderedPairOf(t1,t2)); // Remember the tube
+        cout << (1+t1) << ' ' << (1+t2) << endl; // Output tube
     }
-
     cout << "-1 -1" << endl; // End of input data
 
 }
